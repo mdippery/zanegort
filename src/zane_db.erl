d5348c70be9332fb3f13f7f7f2799a6700ae6f3c
Michael Dippery <michael@monkey-robot.com>
2015-02-19 23:56:53 -0800

    Store saved data in text files
    Eventually I plan to use Mnesia or MongoDB, but for now, text files
    should suffice.

---
+++ src/zane_db.erl
@@ -0,0 +1,45 @@
+-module(zane_db).
+-export([insert/3, find/2]).
+
+
+db_base_dir() -> zane_utils:getenv("ZANE_DB_DIR", "/tmp").
+
+
+db_path(Type) -> filename:join(db_base_dir(), Type).
+
+
+insert(Type, Nickname, Value) ->
+    Path = db_path(Type),
+    Line = string:join([Nickname, Value], ","),
+    file:write_file(Path, Line, [append]).
+
+
+find(Type, Nickname) ->
+    Path = db_path(Type),
+    case file:open(Path, [read]) of
+        {ok, Device} -> find_in_device(Device, Nickname);
+        {error, Reason} -> {error, Reason}
+    end.
+
+
+find_in_device(Device, Nickname) -> find_in_device(Device, Nickname, []).
+
+
+find_in_device(Device, Nickname, Acc) ->
+    case io:get_line(Device, "") of
+        eof ->
+            file:close(Device),
+            find_in_lines(Nickname, Acc);
+        Line ->
+            find_in_device(Device, Nickname, Acc ++ [Line])
+    end.
+
+
+find_in_lines(_Nickname, []) -> nil;
+
+find_in_lines(Nickname, [Head|Rest]) ->
+    [Owner,Value|_] = string:tokens(Head, ","),
+    case Owner of
+        Nickname -> {ok, Value};
+        _ -> find_in_lines(Nickname, Rest)
+    end.
