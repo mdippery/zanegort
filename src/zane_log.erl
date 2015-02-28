16e91e1603bff2870f64b657d8af86376f4af187
Michael Dippery <michael@monkey-robot.com>
2015-02-28 00:34:15 -0800

    Clean up log formatting code

--- src/zane_log.erl
+++ src/zane_log.erl
@@ -2,16 +2,14 @@
 -export([log/3, trace/3]).
 
 
-log(Mod, Msg, Args) ->
-    {{Year,Month,Day}, {Hour,Minute,Second}} = erlang:localtime(),
-    io:format("~p-~2..0B-~2..0B ~2..0B:~2..0B:~2..0B ",
-              [Year, Month, Day, Hour, Minute, Second]),
-    io:format("~p: ", [Mod]),
-    io:format(Msg, Args),
-    io:format("~n").
+log(Mod, Fmt, Args) ->
+    {Date, Time} = erlang:localtime(),
+    Datetime = tuple_to_list(Date) ++ tuple_to_list(Time),
+    Timestamp = io_lib:format("~p-~2..0B-~2..0B ~2..0B:~2..0B:~2..0B", Datetime),
+    Msg = io_lib:format(Fmt, Args),
+    io:format("~s [~p] ~s~n", [Timestamp, Mod, Msg]).
 
 
-
-trace(_Mod, Msg, Args) ->
-    io:format(Msg, Args),
-    io:format("~n").
+trace(_Mod, Fmt, Args) ->
+    Msg = io_lib:format(Fmt, Args),
+    io:format("~s~n", [Msg]).
