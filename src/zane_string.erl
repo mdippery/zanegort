e93d4e895db102bdbb650e26bcc0b2b703ab6226
Michael Dippery <michael@monkey-robot.com>
2015-02-28 02:05:23 -0800

    Strip \001 character from IRC messages
    Technically IRC messages are surrounded by the character \001. I should
    probably be parsing lines based on those characters, and also sending
    those characters around my own messages. Seems to work without, so I'll
    avoid "fixing" that issue for now, but messages I receive from the
    server sometimes _are_ surrounded by \001; my code does not take that
    into account (for ease of use) so I should strip them where they cause
    problems.

--- src/zane_string.erl
+++ src/zane_string.erl
@@ -1,5 +1,7 @@
 -module(zane_string).
--export([strip/1]).
+-export([strip/1, remove_001/1]).
 
 
 strip(S) -> string:strip(string:strip(S, right, $\n), right, $\r).
+
+remove_001(S) -> string:strip(S, both, 1).
