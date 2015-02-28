9577b7c9c882a91a974d69df9d98ca9dc1baa6f9
Michael Dippery <michael@monkey-robot.com>
2015-02-28 01:24:36 -0800

    Extract CTCP commands

--- src/zane_utils.erl
+++ src/zane_utils.erl
@@ -1,7 +1,7 @@
 -module(zane_utils).
 -include("zane.hrl").
 
--export([extract_command/1]).
+-export([extract_command/1, extract_ctcp/3]).
 
 -define(CMD_PREFIX, os_utils:getenv("ZANE_CMD_PREFIX", ?DEFAULT_CMD_PREFIX)).
 
@@ -16,8 +16,19 @@ extract_command(RawCmd, CmdPrefix) ->
     end.
 
 
+extract_ctcp(Client, To, MaybeCtcp=[_|Rest]) ->
+    case is_ctcp(Client, To, MaybeCtcp) of
+        true -> Rest;
+        false -> nil
+    end.
+
+
 is_command(MaybeCmd, CmdPrefix) ->
     case string:substr(MaybeCmd, 1, string:len(CmdPrefix)) of
         CmdPrefix -> true;
         _ -> false
     end.
+
+
+is_ctcp(#irc_client{nickname=Nickname}, Nickname, [P|_Rest]) when P =:= $: -> true;
+is_ctcp(_Client, _Nickname, _Command) -> false.
