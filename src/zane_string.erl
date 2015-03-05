61c757604d7000142bf0ca92aa041fcbf0fc3d21
Michael Dippery <michael@monkey-robot.com>
2015-03-04 17:47:43 -0800

    zane_string:remove_001(): Remove unused function

--- src/zane_string.erl
+++ src/zane_string.erl
@@ -1,7 +1,5 @@
 -module(zane_string).
--export([strip/1, remove_001/1]).
+-export([strip/1]).
 
 
 strip(S) -> string:strip(string:strip(S, right, $\n), right, $\r).
-
-remove_001(S) -> string:strip(S, both, 1).
