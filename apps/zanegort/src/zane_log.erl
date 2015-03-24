4403fedfaf341ba438b1922fb69167055adb7b3d
Michael Dippery <michael@monkey-robot.com>
2015-03-24 13:36:38 -0700

    Export 2-argument logging functions

--- apps/zanegort/src/zane_log.erl
+++ apps/zanegort/src/zane_log.erl
@@ -1,6 +1,8 @@
 -module(zane_log).
--export([log/3, trace/3]).
+-export([log/2, log/3, trace/2, trace/3]).
 
+
+log(Mod, Msg) -> log(Mod, Msg, []).
 
 log(Mod, Fmt, Args) ->
     {Date, Time} = erlang:localtime(),
@@ -8,6 +10,8 @@ log(Mod, Fmt, Args) ->
     Timestamp = io_lib:format("~p-~2..0B-~2..0B ~2..0B:~2..0B:~2..0B", Datetime),
     Msg = io_lib:format(Fmt, Args),
     io:format("~s [~p] ~s~n", [Timestamp, Mod, Msg]).
+
+trace(Mod, Msg) -> trace(Mod, Msg, []).
 
 trace(_Mod, Fmt, Args) ->
     Msg = io_lib:format(Fmt, Args),
