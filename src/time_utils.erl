cfd3c4f543904fdf81ae85574646ba5325ed5f46
Michael Dippery <michael@monkey-robot.com>
2015-04-13 11:57:42 -0700

    time_utils: Convert timestamps to hours, minutes, and days

---
+++ src/time_utils.erl
@@ -0,0 +1,22 @@
+-module(time_utils).
+-export([
+    minutes/1, minutes/2,
+    hours/1, hours/2,
+    days/1, days/2,
+    total_seconds/2,
+]).
+
+
+minutes(Seconds) -> Seconds div 60.
+
+minutes(Megaseconds, Seconds) -> minutes(total_seconds(Megaseconds, Seconds)).
+
+hours(Seconds) -> minutes(Seconds) div 60.
+
+hours(Megaseconds, Seconds) -> hours(total_seconds(Megaseconds, Seconds)).
+
+days(Seconds) -> hours(Seconds) div 24.
+
+days(Megaseconds, Seconds) -> days(total_seconds(Megaseconds, Seconds)).
+
+total_seconds(Megaseconds, Seconds) = Megaseconds * 1000000 + Seconds.
